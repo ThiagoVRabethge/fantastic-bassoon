69c580e9b2b1c11dc7973a2514ad054edcd6bd64
Thiago Volpi Rabethge~ <thiago.rabethge@gmail.com>
2024-09-30 18:15:38 -0300

    minor fix

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,6 +1,6 @@
 import type { MetaFunction } from "@remix-run/node"
 import axios from "axios"
-import { useEffect, useState } from "react"
+import { useState } from "react"
 
 export const meta: MetaFunction = () => {
   return [
@@ -13,12 +13,6 @@ export default function Index() {
   const [name, setName] = useState<string>()
 
   const [email, setEmail] = useState<string>()
-
-  useEffect(() => {
-    axios
-      .get("https://stunning-journey-ashy.vercel.app/users")
-      .then((response) => console.log(response))
-  })
 
   const handleSignIn = async (e: any) => {
     e.preventDefault()
@@ -34,8 +28,6 @@ export default function Index() {
 
   return (
     <div className="flex h-screen items-center justify-center">
-      <span>testing flask api on production only</span>
-
       <form onSubmit={(e) => handleSignIn(e)}>
         <div className="mb-4">
           <input
